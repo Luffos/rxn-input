07b24840f77d2b2dc0dd66a74077bdb8291d2660
Gomes <3GOMESz@gmail.com>
2022-06-05 18:22:38 -0300

    delete unused stuff

--- index.js
+++ index.js
@@ -2,8 +2,4 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export default {};
