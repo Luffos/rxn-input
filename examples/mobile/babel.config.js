ba3d6226dd61f162003968edf4bb15fdfb263e45
Gomes <3gomesz@gmail.com>
2022-07-05 13:27:34 -0300

    tests

--- examples/mobile/babel.config.js
+++ examples/mobile/babel.config.js
@@ -3,4 +3,15 @@ module.exports = {
     'module:metro-react-native-babel-preset',
     ['@babel/preset-typescript', {allowDeclareFields: true}],
   ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react': './node_modules/react',
+          'react-native': './node_modules/react-native',
+        },
+      },
+    ],
+  ],
 };
--- examples/mobile/metro.config.js
+++ examples/mobile/metro.config.js
@@ -6,22 +6,11 @@
  */
 
 const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const moduleRoot = path.resolve(__dirname, '../..');
 
 module.exports = {
   watchFolders: [moduleRoot],
-  resolver: {
-    extraNodeModules: {
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
-    },
-    blockList: exclusionList([
-      new RegExp(`${moduleRoot}/node_modules/react/.*`),
-      new RegExp(`${moduleRoot}/node_modules/react-native/.*`),
-    ]),
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
