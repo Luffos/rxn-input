5790d7822130ac74a81df5bd1bd1019c0ad678d4
Gomes <3gomesz@gmail.com>
2022-07-05 14:09:57 -0300

    tests web example

--- examples/web/craco.config.js
+++ examples/web/craco.config.js
@@ -1,90 +1,94 @@
 /* eslint-disable no-unused-vars */
-const webpack = require("webpack");
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
 
 const TerserPlugin = require('terser-webpack-plugin');
 
 const findWebpackPlugin = (webpackConfig, pluginName) =>
-    webpackConfig.resolve.plugins.find(
-        ({
-            constructor
-        }) => constructor && constructor.name === pluginName
-    );
+  webpackConfig.resolve.plugins.find(
+    ({constructor}) => constructor && constructor.name === pluginName,
+  );
 
 const enableTypescriptImportsFromExternalPaths = (
-    webpackConfig,
-    newIncludePaths
+  webpackConfig,
+  newIncludePaths,
 ) => {
-    const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
-    if (oneOfRule) {
-        const tsxRule = oneOfRule.oneOf.find(
-            (rule) => rule.test && rule.test.toString().includes("tsx")
-        );
+  const oneOfRule = webpackConfig.module.rules.find(rule => rule.oneOf);
+  if (oneOfRule) {
+    const tsxRule = oneOfRule.oneOf.find(
+      rule => rule.test && rule.test.toString().includes('tsx'),
+    );
 
-        if (tsxRule) {
-            tsxRule.include = Array.isArray(tsxRule.include) ? [...tsxRule.include, ...newIncludePaths] : [tsxRule.include, ...newIncludePaths];
-        }
+    if (tsxRule) {
+      tsxRule.include = Array.isArray(tsxRule.include)
+        ? [...tsxRule.include, ...newIncludePaths]
+        : [tsxRule.include, ...newIncludePaths];
     }
+  }
 };
 
 const addPathsToModuleScopePlugin = (webpackConfig, paths) => {
-    const moduleScopePlugin = findWebpackPlugin(
-        webpackConfig,
-        "ModuleScopePlugin"
-    );
-    if (!moduleScopePlugin) {
-        throw new Error(
-            `Expected to find plugin "ModuleScopePlugin", but didn't.`
-        );
-    }
-    moduleScopePlugin.appSrcs = [...moduleScopePlugin.appSrcs, ...paths];
+  const moduleScopePlugin = findWebpackPlugin(
+    webpackConfig,
+    'ModuleScopePlugin',
+  );
+  if (!moduleScopePlugin) {
+    throw new Error(`Expected to find plugin "ModuleScopePlugin", but didn't.`);
+  }
+  moduleScopePlugin.appSrcs = [...moduleScopePlugin.appSrcs, ...paths];
 };
 
 const enableImportsFromExternalPaths = (webpackConfig, paths) => {
-    enableTypescriptImportsFromExternalPaths(webpackConfig, paths);
-    addPathsToModuleScopePlugin(webpackConfig, paths);
+  enableTypescriptImportsFromExternalPaths(webpackConfig, paths);
+  addPathsToModuleScopePlugin(webpackConfig, paths);
 };
 
 module.exports = function () {
-    return {
-        babel: {
-            presets: ["@babel/preset-react"],
-            plugins: [
-                ["react-native-web", {
-                    commonjs: true
-                }],
-                ["replace-imports", {
-                    test: /react$/gi,
-                    replacer: path.resolve("./node_modules/react")
-                }]
-            ],
-        },
-        webpack: {
-            configure: webpackConfig => {
-
-                enableImportsFromExternalPaths(webpackConfig, [
-                    path.resolve(__dirname, "../shared"),
-                    path.resolve(__dirname, "../..")
-                ]);
-
-                return webpackConfig;
+  return {
+    babel: {
+      presets: ['@babel/preset-react'],
+      plugins: [
+        [
+          'react-native-web',
+          {
+            commonjs: true,
+          },
+        ],
+        [
+          'module-resolver',
+          {
+            alias: {
+              react: './node_modules/react',
             },
-            plugins: {
-                add: [
-                    // Inject the "__DEV__" global variable.
-                    new webpack.DefinePlugin({
-                        __DEV__: process.env.NODE_ENV !== "production",
-                    }),
-                    new TerserPlugin({
-                        extractComments: false,
-                        terserOptions: {
-                            format: {
-                                comments: false,
-                            },
-                        },
-                    }),
-                ],
-            }
-        },
-    }
+          },
+        ],
+      ],
+    },
+    webpack: {
+      configure: webpackConfig => {
+        enableImportsFromExternalPaths(webpackConfig, [
+          path.resolve(__dirname, '../shared'),
+          path.resolve(__dirname, '../..'),
+        ]);
+
+        return webpackConfig;
+      },
+      plugins: {
+        add: [
+          // Inject the "__DEV__" global variable.
+          new webpack.DefinePlugin({
+            __DEV__: process.env.NODE_ENV !== 'production',
+          }),
+          new TerserPlugin({
+            extractComments: false,
+            terserOptions: {
+              format: {
+                comments: false,
+              },
+            },
+          }),
+        ],
+      },
+    },
+  };
 };
