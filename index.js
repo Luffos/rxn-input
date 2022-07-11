deb0d1155e6692af3b05ce9d2d9ebf94e88f21b6
Gomes <3gomesz@gmail.com>
2022-07-11 19:53:18 -0300

    change KeyCode

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 
 import {NativeModules} from 'react-native';
 import KeyListener, {KeyEvent} from './src/KeyListener';
+import KeyCode from './src/KeyCode';
 
 const {RXNKeyListener} = NativeModules;
 
-export {RXNKeyListener, KeyEvent};
+export {RXNKeyListener, KeyEvent, KeyCode};
 
 export default KeyListener;
