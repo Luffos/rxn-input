09fdfdf09649087bfa1b4bb46f330fe0c6c55e81
Gomes <3gomesz@gmail.com>
2022-07-11 20:12:52 -0300

    Updae AndroidKeyCode

--- src/AndroidKeyCode.ts
+++ src/AndroidKeyCode.ts
@@ -1,3 +1,4 @@
+// http://www.temblast.com/ref/akeyscode.htm
 enum AndroidKeyCode {
   UNKNOWN = 0,
   SOFT_LEFT = 1,
@@ -9,6 +10,15 @@ enum AndroidKeyCode {
   '_0' = 7,
   '_1' = 8,
   '_2' = 9,
+  '_3' = 10,
+  '_4' = 11,
+  '_5' = 12,
+  '_6' = 13,
+  '_7' = 14,
+  '_8' = 15,
+  '_9' = 16,
+  STAR = 17,
+  POUND = 18,
 }
 
 enum AndroidKeyCodeString {
@@ -22,6 +32,15 @@ enum AndroidKeyCodeString {
   '_0' = '0',
   '_1' = '1',
   '_2' = '2',
+  '_3' = '3',
+  '_4' = '4',
+  '_5' = '5',
+  '_6' = '6',
+  '_7' = '7',
+  '_8' = '8',
+  '_9' = '9',
+  STAR = 'START',
+  POUND = 'POUND',
 }
 
 export {AndroidKeyCode, AndroidKeyCodeString};
