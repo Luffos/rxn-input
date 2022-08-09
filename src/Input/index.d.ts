604a7a933db2d331dd6d6e334d5b2618b42e746e
Gomes <3gomesz@gmail.com>
2022-08-09 19:52:54 -0300

    add stuff

--- src/Input/index.d.ts
+++ src/Input/index.d.ts
@@ -5,15 +5,24 @@ export interface iInputFunctions {
 
 export interface iInputVariables {
   /**
-   * Returns if touch inputs are avaible, which maybe supported by device but that's not in use at moment.
+   * Returns if touch inputs are present, which maybe supported by device but that's not in use at moment.
    *
    * Can change on desktop web browsers, when toggle mobile simulation tools.
    *
-   * @hook useTouchAvaible
-   * @event TouchListener.on('changeTouchAvaible')
+   * @hook useTouchPresent
+   * @event TouchListener.on('changeTouchPresence')
    * @returns boolean
    */
-  touchAvaible: boolean;
+  touchPresent: boolean;
+
+  /**
+   * Returns if a mouse device is detected / connected.
+   *
+   * @hook useMousePresent
+   * @event MouseListener.on('changeMousePresence')
+   * @returns boolean
+   */
+  mousePresent: boolean;
 }
 
 type iInput = iInputFunctions & iInputVariables;
---
+++ src/hooks/useMousePresent/index.web.tsx
@@ -0,0 +1,1 @@
+// matchMedia('(pointer:fine)').matches;
