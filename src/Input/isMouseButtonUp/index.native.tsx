6a4ae5f6e23d556751f46dfa427c31ab4d9e8e65
Lucas <lgxm3z@gmail.com>
2022-08-14 15:02:18 -0300

    interface changes

---
+++ src/Input/isMouseButtonUp/index.native.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type iType from '.';
+
+const isMouseButtonUp: typeof iType = button => {
+  return false;
+};
+
+export default isMouseButtonUp;
