22ef579e8c3595a8d8805f4e64834e2fca6038ca
Lucas <lgxm3z@gmail.com>
2022-08-30 19:45:42 -0300

    create folders

--- website/src/pages/docs/index.tsx
+++ website/src/pages/docs/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import type {HeadFC} from 'gatsby';
-import Layout from '../components/Layout';
+import Layout from '../../components/Layout';
 
 const DocsPage = () => {
   return (
