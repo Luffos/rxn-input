5b0f950a02112f312be630d257bc654918b182a2
Lucas <lgxm3z@gmail.com>
2022-08-30 17:28:35 -0300

    change gatsby-config.ts

--- website/gatsby-config.ts
+++ website/gatsby-config.ts
@@ -2,13 +2,13 @@ import type {GatsbyConfig} from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://luffos.github.io/rxn-input`
+    siteUrl: `https://luffos.github.io/rxn-input/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: '/rxn-input',
+  pathPrefix: '/rxn-input/',
   plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-sitemap`]
 };
 
