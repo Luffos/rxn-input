eb9d3d0131f5e96dd9657119f29b9edc9f76f4b0
Lucas <lgxm3z@gmail.com>
2022-08-30 20:16:23 -0300

    update Layout

--- website/src/components/Layout/index.tsx
+++ website/src/components/Layout/index.tsx
@@ -6,15 +6,15 @@ import Footer from '../Footer';
 interface iProps {
   children?: any;
   TopBarSelected?: iTopBarSelected;
-  PageHeight?: string;
+  PageMinHeight?: string;
 }
 
-const Layout = ({children, TopBarSelected, PageHeight}: iProps) => {
+const Layout = ({children, TopBarSelected, PageMinHeight}: iProps) => {
   return (
     <>
       <TopNavBar TopBarSelected={TopBarSelected} />
       <div style={{display: 'flex', flexDirection: 'column'}}>
-        <div style={{width: '100%', height: PageHeight || '100vh'}}>{children}</div>
+        <div style={{width: '100%', minHeight: PageMinHeight || '100vh'}}>{children}</div>
         <Footer />
       </div>
     </>
