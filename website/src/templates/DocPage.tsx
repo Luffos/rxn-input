00ec4c6a7ee27aa224a2f425c73fa16f4fe69bb0
Lucas <lgxm3z@gmail.com>
2022-09-22 05:51:58 -0300

    tests

--- website/src/templates/DocPage.tsx
+++ website/src/templates/DocPage.tsx
@@ -1,17 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, {useEffect} from 'react';
 import Layout from '../components/Layout';
-import {MDXProvider} from '@mdx-js/react';
-import {graphql, useStaticQuery} from 'gatsby';
 
-import c from '../../public/docs/test.json';
+import * as c from '../../public/docs/test.json';
 
 function DocPage({pageContext, children}: any) {
   const context = pageContext;
 
-
   console.log(context);
-  
   console.log(c);
 
   return (
