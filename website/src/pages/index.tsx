291969442496ecc103c72da61168d93929f279c8
Lucas Gomes <lgxm3z@gmail.com>
2022-09-28 17:05:52 -0300

    test

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,7 +7,8 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <LayoutContent>
-        <h1 style={{margin: 0, padding: 0}}>Home</h1>
+        <h1 style={{margin: 0, padding: 0}}>Cross-Platform Input Handler for React and React Native.</h1>
+        <h2>Mouse, Touches, Keyboard and Gamepad</h2>
       </LayoutContent>
     </Layout>
   );
