8007fea2d3cbf6b58d6f80410bbf49aeeae38dd6
Lucas <lgxm3z@gmail.com>
2022-10-04 17:55:41 -0300

    [website] improvements

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { useState, useLayoutEffect } from 'react';
 import type { NextPage } from 'next';
 import Layout, { LayoutContent } from '../components/Layout';
-import { Title, SubTitle, DevicesWrapper, Section1, GetStartedButton } from '../styles/pages/index.styles';
+import { Title, SubTitle, DevicesWrapper, Section1Content, GetStartedButton, Section1Background, Section1 } from '../styles/pages/index.styles';
 import DeviceSVG from '../assets/Devices.svg';
 import Head from 'next/head';
 import useMediaQuery from '../hooks/useMediaQuery';
@@ -10,6 +11,12 @@ import Link from 'next/link';
 const Home: NextPage = () => {
   const downLg = useMediaQuery(BreakPoints.down('lg'));
 
+  const [showSection1, setShowSection1] = useState(false);
+
+  useLayoutEffect(() => {
+    setShowSection1(true);
+  }, []);
+
   return (
     <>
       <Head>
@@ -18,35 +25,42 @@ const Home: NextPage = () => {
 
       <Layout>
         <LayoutContent style={{ marginTop: `9rem` }}>
-          <Section1>
-            <div>
-              <Title>
-                Cross-Platform Input Handler
-                <br />
-                for React and React Native
-              </Title>
-              <SubTitle>
-                Mouse<span>,</span> Touches<span>,</span> Keyboard <span>and</span> Gamepad
-              </SubTitle>
-              {!downLg && (
-                <Link href={'/docs/getting-started/installation'}>
-                  <a style={{ display: downLg ? 'none' : 'flex', width: 'fit-content' }}>
-                    <GetStartedButton>Get started</GetStartedButton>
-                  </a>
-                </Link>
-              )}
-            </div>
-            <DevicesWrapper>
-              <DeviceSVG viewBox="0 0 469 298" />
-            </DevicesWrapper>
-            {downLg && (
-              <Link href={'/docs/getting-started/installation'}>
-                <a style={{ display: !downLg ? 'none' : 'flex', width: 'fit-content', margin: '0 auto' }}>
-                  <GetStartedButton>Get started</GetStartedButton>
-                </a>
-              </Link>
-            )}
-          </Section1>
+          {showSection1 && (
+            <Section1>
+              <Section1Background />
+              <div style={{ position: 'relative' }}>
+                <Section1Content>
+                  <div>
+                    <Title>
+                      Cross-Platform Input Handler
+                      <br />
+                      for React and React Native
+                    </Title>
+                    <SubTitle>
+                      Mouse<span>,</span> Touches<span>,</span> Keyboard <span>and</span> Gamepad
+                    </SubTitle>
+                    {!downLg && (
+                      <Link href={'/docs/getting-started/installation'}>
+                        <a style={{ display: downLg ? 'none' : 'flex', width: 'fit-content' }}>
+                          <GetStartedButton>Get started</GetStartedButton>
+                        </a>
+                      </Link>
+                    )}
+                  </div>
+                  <DevicesWrapper>
+                    <DeviceSVG viewBox="0 0 469 298" />
+                  </DevicesWrapper>
+                  {downLg && (
+                    <Link href={'/docs/getting-started/installation'}>
+                      <a style={{ display: !downLg ? 'none' : 'flex', width: 'fit-content', margin: '0 auto' }}>
+                        <GetStartedButton>Get started</GetStartedButton>
+                      </a>
+                    </Link>
+                  )}
+                </Section1Content>
+              </div>
+            </Section1>
+          )}
         </LayoutContent>
       </Layout>
     </>
